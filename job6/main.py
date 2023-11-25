87131628ca82d6d315c78ba1bff2cacf5117722e
magomed-malik-issaev <146710709+magomed-malik-issaev@users.noreply.github.com>
2023-11-25 12:55:33 +0100

    Delete job6 directory

--- job6/main.py
+++
@@ -1,7 +0,0 @@
-# Programme stockant une phrase dans une variable et l'affichant
-
-# Stocker la phrase dans la variable ma_string
-ma_string = "je suis une String"
-
-# Afficher la variable dans le terminal
-print(ma_string)
